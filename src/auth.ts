91cc46c7f1f4c83518976c6ee5e2da2becfe2393
Hemi Phillips <hello.hemi.phillips@gmail.com>
2026-01-11 06:25:38 +1300

    Configure the auth client

--- src/auth.ts
+++ src/auth.ts
@@ -1,3 +1,9 @@
-import { createAuthClient } from '@neondatabase/auth'
+import { createAuthClient } from '@neondatabase/neon-js/auth'
+import { BetterAuthReactAdapter } from '@neondatabase/neon-js/auth/react'
 
-export const authClient = createAuthClient(import.meta.env.VITE_NEON_AUTH_URL)
+const VITE_NEON_AUTH_URL = import.meta.env.VITE_NEON_AUTH_URL
+if (!VITE_NEON_AUTH_URL) {
+  throw new Error('VITE_NEON_AUTH_URL is not set')
+}
+
+export const authClient = createAuthClient(VITE_NEON_AUTH_URL, { adapter: BetterAuthReactAdapter() })
