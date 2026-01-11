208cafba8c7a4a922d0b3c6c91c3b8bb314f5b71
Hemi Phillips <hello.hemi.phillips@gmail.com>
2026-01-12 11:12:55 +1300

    Add util for handling empty strings from forms

---
+++ src/lib/empty-strings.ts
@@ -0,0 +1,39 @@
+import { z } from 'zod'
+
+/**
+ * Allows a string schema to accept either a valid string (per the schema) or an empty string.
+ * Useful for optional fields that use empty strings as "null" values (converted to null before DB).
+ *
+ * @example
+ * optionalField(z.string().trim().min(1)) // 'abc' or ''
+ */
+export function optionalField<T extends z.ZodType<string>>(schema: T) {
+  return z.union([schema, z.literal('')])
+}
+
+/**
+ * Converts an empty string to null.
+ * Useful for converting empty strings to null before writing to the database.
+ *
+ * @example
+ * emptyStringToNull('') // null
+ * emptyStringToNull('hello') // 'hello'
+ */
+export function emptyStringToNull<T>(value: T): T | null {
+  return value === '' ? null : value
+}
+
+type NullToEmpty<T> = T extends null ? '' : T
+
+/**
+ * Converts a null value to an empty string.
+ * Useful for converting null values to empty strings for form inputs.
+ *
+ * @example
+ * nullToEmptyString(null) // ''
+ * nullToEmptyString('hello') // 'hello'
+ */
+export function nullToEmptyString<T>(value: T): NullToEmpty<T> {
+  // we can safely cast because we know that null is the only value that can be converted to an empty string
+  return (value === null ? '' : value) as NullToEmpty<T>
+}
