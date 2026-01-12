cb39f83e76a1b29719cda69a159f6abf3a034b1d
Hemi Phillips <hello.hemi.phillips@gmail.com>
2026-01-12 16:02:09 +1300

    Update lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,24 @@
 
 import { tanstackConfig } from '@tanstack/eslint-config'
 
-export default [...tanstackConfig]
+export default [
+  ...tanstackConfig,
+  {
+    rules: {
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'typeParameter',
+          format: ['PascalCase', 'UPPER_CASE'],
+          // Allow single-letter type parameters like E, T, U (common convention)
+          filter: {
+            regex: '^[A-Z]$',
+            match: true,
+          },
+        },
+      ],
+      // Disable unnecessary condition check - shadcn components use defensive optional chaining
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+    },
+  },
+]
