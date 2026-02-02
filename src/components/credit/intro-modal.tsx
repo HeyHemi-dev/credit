d9cef17404fa1ebd303957ca2686a3af22acb511
Hemi Phillips <hello.hemi.phillips@gmail.com>
2026-02-03 10:48:47 +1300

    Refactor IntroModal to utilize local storage utility for dismissal state management, integrating context for eventId and improving state handling.

--- src/components/credit/intro-modal.tsx
+++ src/components/credit/intro-modal.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from '@tanstack/react-router'
 import {
   AlertDialog,
   AlertDialogAction,
@@ -9,24 +10,24 @@ import {
 } from '@/components/ui/alert-dialog'
 import { tryCatchSync } from '@/lib/try-catch'
 import { Brand } from '@/components/header'
-
-const STORAGE_KEY = 'credit.coupleIntroDismissed.v1'
+import { shareIntroModalDismissed } from '@/lib/local-storage'
+import { useCreditContext } from '@/contexts/credit-page-context'
 
 export function IntroModal() {
   // TODO: add notice about agreeing to the terms of service and privacy policy (By using this link, you agree to our [Terms] and [Privacy Policy]")
 
+  const { eventId } = useCreditContext()
   const [open, setOpen] = React.useState(false)
+  const { getValue, setValue } = shareIntroModalDismissed(eventId)
 
   React.useEffect(() => {
-    const { data: dismissed } = tryCatchSync(() =>
-      window.localStorage.getItem(STORAGE_KEY),
-    )
+    const { data: dismissed } = tryCatchSync(getValue)
     if (dismissed) return
     setOpen(true)
   }, [])
 
   function dismiss() {
-    tryCatchSync(() => window.localStorage.setItem(STORAGE_KEY, '1'))
+    tryCatchSync(() => setValue('1'))
     setOpen(false)
   }
 
@@ -48,7 +49,7 @@ export function IntroModal() {
           </div>
         </AlertDialogHeader>
 
-        <AlertDialogFooter className="flex justify-end">
+        <AlertDialogFooter>
           <AlertDialogAction onClick={dismiss} className="min-w-24">
             Got it
           </AlertDialogAction>
