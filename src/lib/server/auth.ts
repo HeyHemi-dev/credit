f0fee70804db91b1a23212b1d79ee816d74a198a
Hemi Phillips <hello.hemi.phillips@gmail.com>
2026-03-12 19:48:27 +1300

    Validate session expiration

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -21,7 +21,7 @@ export async function isValidAuthToken(authToken: AuthToken) {
   }
 
   if (authToken.tokenType === 'sessionToken') {
-    const isValid = await isValidSession(authToken.token)
+    const isValid = await isValidSession()
     if (!isValid) return false
   }
 
